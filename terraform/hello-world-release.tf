372357f9d79b0d70c4346b1edde9375aa0cb8c6c
msmith93 <matthew.k.smith12@gmail.com>
2021-10-04 09:38:29 -0700

    bump chart version

--- terraform/hello-world-release.tf
+++ terraform/hello-world-release.tf
@@ -16,10 +16,6 @@ resource "helm_release" "hello_world_release" {
   chart           = "helloworld"
   cleanup_on_fail = true
 
-  values = [
-    "${file("values.yaml")}"
-  ]
-
   set {
     name  = "cluster.enabled"
     value = "true"
@@ -27,10 +23,10 @@ resource "helm_release" "hello_world_release" {
 
   set {
     name = "ingress_cert"
-    #value = "arn:aws:acm:us-west-2:148660894061:certificate/b985d09b-6622-4525-aacc-78b53002cddd" # aws_acm_certificate.cert.arn
     value = "arn:aws:acm:us-west-2:148660894061:certificate/c3a4d137-6457-4787-9401-498f6b66ea18"
   }
 
   # Ensure helm release is not installed before ALB ingress controller is installed
   depends_on = [module.alb_ingress_controller.aws_iam_role_arn]
 }
+
