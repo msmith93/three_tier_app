0c74be0866499d7bc763ceed6559ebce43b5756a
msmith93 <matthew.k.smith12@gmail.com>
2021-10-04 09:04:41 -0700

    Bump chart version. Remove ECR

--- terraform/ecr-repository.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_ecr_repository" "helloworldrepo" {
-  name                 = "helloworld"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
